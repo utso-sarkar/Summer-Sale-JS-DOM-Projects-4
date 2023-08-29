0b9b399e5a753d82daafd26279f0acdfb0876e0e
Utso-Sarkar <devutso428@gmail.com>
2023-08-29 18:32:23 +0600

    add com

---
+++ script.js
@@ -0,0 +1,11 @@
+// reuseableFunction
+function calculateProduct(){
+    console.log("lala la la la");
+}
+
+
+
+// add to calculation entry
+function addToCalculationEntry(){
+    console.log("add to entry");
+}
